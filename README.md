9cbd9679079961537fd5d953d37e9cea2a5b0bc7
Galeel Bhasha Satthar <galeels-ttltw@thetrainline.com>
2018-01-23 19:08:29 +0530

    updated build task and readme file

--- README.md
+++ README.md
@@ -1,12 +1,30 @@
 # String Utility Methods
 Utility functions for string operations
 
-## Usage
-```js
-import strUtils from  'js-string-utils'
+## Installation
+To install the stable version you can use [npm](https://npmjs.org/) or [yarn](https://yarnpkg.com/en/):
+
+Using npm:
+```shell
+$ npm i js-string-utils --save
 ```
 
-## Methods
+Using yarn:
+```shell
+$ yard add js-string-utils
+```
+
+## Usage
+Include into your javascript file:
+```js
+import strUtils from  'js-string-utils';
+
+strUtils.toCamelCase('Hello World'); // returns 'helloWorld'
+
+strUtils.toTitleCase('HELLO WORLD'); // returns 'Hello World'
+```
+
+## Available Methods
 
 .randomString()
 
--- build.js
+++ build.js
@@ -2,15 +2,18 @@ var fs = require('fs-extra');
 var path = require('path');
 var cp = require('child_process');
 var chalk = require('chalk');
+
 var srcPath = path.resolve('src')
 var srcPackageJSONPath = path.join(srcPath, '..', 'package.json');
 var srcReadMePath = path.join(srcPath, '..', 'README.md');
+
 var libPath = path.resolve('lib');
 var libPackageJSONPath = path.join(libPath, 'package.json');
 var libReadMePath = path.join(libPath, 'README.md');
+
 function build_publish() {
-    console.log(chalk.green("Babel task convert ES6 to Es5 started"));
-    cp.exec('babel ' + srcPath + ' -d ' + libPath + ' --ignore __tests__, --copy-files', function (error, stdout, stderr) {
+    console.log(chalk.green("Babel task to convert ES6 to ES5 started...."));
+    cp.exec('babel ' + srcPath + ' -d ' + libPath + ' --ignore __tests__', function (error, stdout, stderr) {
         if (error) {
             console.log(chalk.red("---errors---", error));
             return;
@@ -20,13 +23,15 @@ function build_publish() {
             return;
         }
         console.log("stdout", stdout)
-        console.log(chalk.green("Babel task convert ES6 to Es5 done"))
+        console.log(chalk.green("Babel task to convert ES6 to ES5 done !!!"))
+
+        console.log(chalk.green("copying files..."))
         fs.copy(srcPackageJSONPath, libPackageJSONPath)
-        .then(() => console.log(chalk.green("pacakge.json copied sucessfully!")))
+        .then(() => console.log(chalk.green("pacakge.json file copied sucessfully!")))
         .catch(err => console.error(err))
 
         fs.copy(srcReadMePath, libReadMePath)
-        .then(() => console.log(chalk.green("README.md copied sucessfully!")))
+        .then(() => console.log(chalk.green("README.md file copied sucessfully!")))
         .catch(err => console.error(err))
     })
 }
